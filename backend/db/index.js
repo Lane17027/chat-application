9cb61a9a16482dea0849c4e4c0ed94debf2a3adf
Lane17027 <lanenichols17027@gmail.com>
2024-05-22 13:22:12 -0600

    setting up base

---
+++ backend/db/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// let uri = "mongodb+srv://Lane17027:Lane17027@cmonluster0.edoucjb.mongodb.net/?appName=Cluster0"
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log(`Successfully connected to ${uri}`);
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
+
+ mongoose.set('debug', true)
+const db = mongoose.connection;
+
+module.exports = db;
